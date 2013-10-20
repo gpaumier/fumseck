f0dfa5000cd52bb7398130496e39bb4d6955b642
Guillaume Paumier <guillaume@gpaumier.org>
2013-10-20 14:15:51 +0200

    Include custom content to search & archives
    * Modify the WP query to include custom content types (Projects and Events) to search results and archive pages

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+# Modify the WP query to include custom content types to search results
+# and archive pages
+
+add_filter( 'pre_get_posts', 'fumseck_add_cpt_search' );
+
+function fumseck_add_cpt_search( $query ) {
+    if ( $query->is_search || $query->is_archive )
+        $query->set( 'post_type', array( 'post', 'batbelt_project', 'batbelt_event' ) );
+    return $query;
+}
+
+?>
