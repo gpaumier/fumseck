e402f72e4d6bade810ded7c7e3c4cc1b9c66a99d
Guillaume Paumier <gp@guillaumepaumier.com>
2013-10-24 16:57:37 +0200

    Init css and header for WordPress

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title><?php wp_title(); ?></title>
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+        <?php wp_head(); ?>
     </head>
     <body>
         <?php
