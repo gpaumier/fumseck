778d7e699c4f09bc9e8adc18de41e3b98974a5c8
Guillaume Paumier <gp@guillaumepaumier.com>
2013-10-24 17:50:33 +0200

    Move header to dedicated file

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <title><?php wp_title(); ?></title>
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <?php wp_head(); ?>
+    </head>
+    <body>
--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <title><?php wp_title(); ?></title>
-        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-        <?php wp_head(); ?>
-    </head>
-    <body>
-        <?php
-        
-        ?>
+<?php get_header(); ?>
+
     </body>
 </html>
