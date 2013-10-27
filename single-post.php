9e552fe7aa2a060d4960ecc2bf4af23c145f95a8
Guillaume Paumier <gp@guillaumepaumier.com>
2013-10-27 19:59:35 +0100

    Add empty placeholder PHP templates for single posts

--- single-post.php
+++ single-post.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
 
+				<div id="content" role="main">
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'content', get_post_format() ); ?>
+						<?php get_template_part( 'syndication' ); ?>
+
+					<?php endwhile; // end of The Loop. ?>
+
+				</div><!-- #content -->
+
 <?php get_footer(); ?>
