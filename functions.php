922e0759e142365b13f8b689d48e1505edd2bce2
Guillaume Paumier <gp@guillaumepaumier.com>
2013-11-02 16:55:36 +0100

    Declare theme support for WordPress features

--- functions.php
+++ functions.php
@@ -1,9 +1,17 @@
 <?php
 
-# Modify the WP query to include custom content types to search results
-# and archive pages
+// Declare WordPress features the theme explicitly supports
 
-# FIXME This shows all three kinds of posts in the back-end
+add_theme_support( 'post-formats', array( 'image' ) );
+
+add_theme_support( 'post-thumbnails' );
+
+add_theme_support( 'automatic-feed-links' );
+
+add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form' ) );
+
+// Modify the WP query to include custom content types to search results
+// and archive pages
 
 add_filter( 'pre_get_posts', 'fumseck_add_cpt_search' );
 
@@ -13,14 +21,14 @@ function fumseck_add_cpt_search( $query ) {
 	return $query;
 }
 
-# Return the linked title of a given post object
+// Return the linked title of a given post object
 
 function fumseck_linked_title( $id ) {
 	$post_title = get_the_title( $id );
 	echo '<a href="' . esc_url( get_permalink( $id ) ) . '" title="' . esc_attr( $post_title, 'fumseck' ) . '">'. $post_title . '</a>' ;
 }
 
-# Display the default image for content without a featured image
+// Display the default image for content without a featured image
 
 function fumseck_default_image( $post_type ) {
 	// TODO
