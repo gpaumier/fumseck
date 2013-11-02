83f0e8b9f01957f7f73893815a39bc466fc9e910
Guillaume Paumier <gp@guillaumepaumier.com>
2013-11-02 12:08:10 +0100

    Mostly working responsive layout for image posts
    * Horizontally and vertically responsive

--- content-image.php
+++ content-image.php
@@ -7,16 +7,16 @@
 	?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( $ratio) ; ?>>
-	
-	<header><h1><?php the_title(); ?></h1></header>
-	
+
 	<figure>
 		<?php the_post_thumbnail(); ?>
 		<figcaption><?php the_title(); ?></figcaption>
 	</figure>
 	
-	<aside>
-		<ul class="image-meta">
+	<header><h1><?php the_title(); ?></h1></header>
+	
+	<aside class="image-meta">
+		<ul>
 		
 			<?php if ( $featured_image_exif ) {
 					if ( $exif_date_taken = $featured_image_exif['image_meta']['created_timestamp'] ) {?>
@@ -49,29 +49,44 @@
 		</ul>
 	</aside>
 	
-	<?php the_content(); ?>
+	<div class="image-summary">
+		<?php // Display the excerpt unless there's an image summary
+			if ( $image_summary = get_field( '_image_summary', get_the_ID(), true ) ) {
+				echo $image_summary;
+			} else {
+				the_excerpt();
+			}; ?></div>
 	
-	<aside>
-		<ul class="image-meta">
+	<aside class="image-pub-meta">
+		<ul>
 			<li><span class="label"><?php _e( 'published on: ' , 'fumseck' ); ?></span><time datetime="<?php echo esc_attr( get_the_date( 'c' ) ) ; ?>" pubdate><?php echo date_i18n( __('F j, Y', 'fumseck'), get_the_date('U') ) ; ?></time></li>
 			
-			<?php if ( $categories_list = get_the_category_list( __( ', ', 'fumseck' ) ) ) :?>
+			<?php if ( $categories_list = get_the_category_list( __( ', ', 'fumseck' ) ) ) {?>
 			<li><span class="label"><?php _ex( 'in: ' , 'in categories', 'fumseck' ); ?></span><?php echo $categories_list ; ?></li>
-			<?php endif; ?>
+			<?php }; ?>
 		
-			<?php if ( $tags_list = get_the_tag_list( '', __( ', ', 'fumseck' ) ) ) : ?>
+			<?php if ( $tags_list = get_the_tag_list( '', __( ', ', 'fumseck' ) ) ) { ?>
 			<li><span class="label"><?php _e( 'topics: ' , 'fumseck' ); ?></span><?php echo $tags_list ?></li>
-			<?php endif; ?>
+			<?php }; ?>
 		
-			<?php if ( $batbelt_people_list = get_the_term_list( get_the_ID(), 'batbelt_people', '', __( ', ', 'fumseck' ) ) ) :?>
+			<?php if ( $batbelt_people_list = get_the_term_list( get_the_ID(), 'batbelt_people', '', __( ', ', 'fumseck' ) ) ) {?>
 			<li><span class="label"><?php _e( 'people: ' , 'fumseck' ); ?></span><?php echo $batbelt_people_list ; ?></li>
-			<?php endif; ?>
+			<?php }; ?>
 		
-			<?php if ( $batbelt_project = get_field( '_project', get_the_ID(), true ) ) :?>
+			<?php if ( $batbelt_project = get_field( '_project', get_the_ID(), true ) ) {?>
 			<li><span class="label"><?php _e( 'project: ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_project ); ?></li>
-			<?php endif; ?>
+			<?php } ?>
 		</ul>
 	</aside>
+	
+	<?php // TODO add syndication here ?>
+	
+	<?php // Only display the full content if there's more than the excerpt
+	if ( $full_content = get_the_content() ) {
+		$full_content = apply_filters('the_content', $full_content);
+		$full_content = str_replace(']]>', ']]&gt;', $full_content);?>
+	<div class="the-content"><?php echo $full_content; ?></div>
+	<?php }; ?>
 	
 </article><!-- #post -->
 
