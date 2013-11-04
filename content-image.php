fb0ae834680e146b752d3b21c7b183050d22d77b
Guillaume Paumier <gp@guillaumepaumier.com>
2013-11-04 20:38:38 +0100

    Mostly-working integration of Font Awesome
    * Custom implementation with a mixin to avoid butchering the HTML markup
    * Icons are stored in variables in the partial provided with the font for future-proofing
    * List icons can be applied directly to <li> items
    * Also made minor tweaks to the markup to adjust

--- content-image.php
+++ content-image.php
@@ -15,34 +15,34 @@
 	
 	<header><h1><?php the_title(); ?></h1></header>
 	
-	<aside class="image-meta">
-		<ul>
+	<aside>
+		<ul class="image-meta">
 		
 			<?php if ( $featured_image_exif ) {
 					if ( $exif_date_taken = $featured_image_exif['image_meta']['created_timestamp'] ) {?>
-			<li><span class="label date_taken opt"><?php _e( 'Taken on ' , 'fumseck' ); ?></span><time datetime="<?php echo esc_attr( date( 'c', $exif_date_taken ) ); ?>"><?php echo date_i18n( __("F j, Y", 'fumseck'), $exif_date_taken ) ; ?></time></li>
+			<li class="date_taken"><span class="label opt"><?php _e( 'Taken on ' , 'fumseck' ); ?></span><time datetime="<?php echo esc_attr( date( 'c', $exif_date_taken ) ); ?>"><?php echo date_i18n( __("F j, Y", 'fumseck'), $exif_date_taken ) ; ?></time></li>
 					<?php };?>
 			<?php };?>
 			
 			<?php if ( $batbelt_location = get_the_term_list( get_the_ID(), 'batbelt_locations', '', __( ', ', 'fumseck' ) ) ) {?>
-			<li><span class="label location opt"><?php _ex( 'in ' , 'in location', 'fumseck' ); ?></span><?php printf( $batbelt_location ); ?></li>
+			<li class="location"><span class="label opt"><?php _ex( 'in ' , 'in location', 'fumseck' ); ?></span><?php printf( $batbelt_location ); ?></li>
 			<?php };?>
 			
 			<?php if ( $batbelt_event = get_field( '_event', get_the_ID(), true ) ) {?>
-			<li><span class="label event"><?php _e( 'during: ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_event ); ?></li>
+			<li class="event"><span class="label"><?php _e( 'during ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_event ); ?></li>
 			<?php };?>
 			
 			<?php if ( $featured_image_exif ) {
 				if ( $exif_aperture = $featured_image_exif['image_meta']['aperture'] ) {?>
-			<li><span class="label aperture opt"><?php _e( 'Aperture: ' , 'fumseck' ); ?></span><?php echo '<i>ƒ</i>/' . sanitize_text_field( $exif_aperture ) ; ?></li>
+			<li class="aperture"><span class="label opt"><?php _e( 'Aperture: ' , 'fumseck' ); ?></span><?php echo '<i>ƒ</i>/' . sanitize_text_field( $exif_aperture ) ; ?></li>
 				<?php };?>
 			
 				<?php if ( $exif_focal_length = $featured_image_exif['image_meta']['focal_length'] ) {?>
-			<li><span class="label focal_length opt"><?php _e( 'Focal length: ' , 'fumseck' ); ?></span><?php echo sanitize_text_field( $exif_focal_length ) . ' mm' ; ?></li>
+			<li class="focal_length"><span class="label opt"><?php _e( 'Focal length: ' , 'fumseck' ); ?></span><?php echo sanitize_text_field( $exif_focal_length ) . ' mm' ; ?></li>
 				<?php };?>
 			
 				<?php if ( $exif_exposure = $featured_image_exif['image_meta']['shutter_speed'] ) {?>
-			<li><span class="label exposure opt"><?php _e( 'Exposure time: ' , 'fumseck' ); ?></span><?php echo sanitize_text_field( $exif_exposure ) . ' s' ; ?></li>
+			<li class="exposure"><span class="label opt"><?php _e( 'Exposure time: ' , 'fumseck' ); ?></span><?php echo sanitize_text_field( $exif_exposure ) . ' s' ; ?></li>
 				<?php };?>
 			<?php }; # end exif?>
 		
@@ -57,24 +57,24 @@
 				the_excerpt();
 			}; ?></div>
 	
-	<aside class="image-pub-meta">
-		<ul>
-			<li><span class="label pub_date"><?php _e( 'Published on ' , 'fumseck' ); ?></span><time datetime="<?php echo esc_attr( get_the_date( 'c' ) ) ; ?>" pubdate><?php echo date_i18n( __('F j, Y', 'fumseck'), get_the_date('U') ) ; ?></time></li>
+	<aside>
+		<ul class="image-pub-meta">
+			<li class="pub_date"><span class="label"><?php _e( 'Published on ' , 'fumseck' ); ?></span><time datetime="<?php echo esc_attr( get_the_date( 'c' ) ) ; ?>" pubdate><?php echo date_i18n( __('F j, Y', 'fumseck'), get_the_date('U') ) ; ?></time></li>
 			
 			<?php if ( $categories_list = get_the_category_list( __( ', ', 'fumseck' ) ) ) {?>
-			<li><span class="label categories"><?php _ex( 'In: ' , 'in categories', 'fumseck' ); ?></span><?php echo $categories_list ; ?></li>
+			<li class="categories"><span class="label"><?php _ex( 'in ' , 'in categories', 'fumseck' ); ?></span><?php echo $categories_list ; ?></li>
 			<?php }; ?>
 		
 			<?php if ( $tags_list = get_the_tag_list( '', __( ', ', 'fumseck' ) ) ) { ?>
-			<li><span class="label topics"><?php _e( 'Topics: ' , 'fumseck' ); ?></span><?php echo $tags_list ?></li>
+			<li class="topics"><span class="label"><?php _e( 'Topics: ' , 'fumseck' ); ?></span><?php echo $tags_list ?></li>
 			<?php }; ?>
 		
 			<?php if ( $batbelt_people_list = get_the_term_list( get_the_ID(), 'batbelt_people', '', __( ', ', 'fumseck' ) ) ) {?>
-			<li><span class="label people"><?php _e( 'People: ' , 'fumseck' ); ?></span><?php echo $batbelt_people_list ; ?></li>
+			<li class="people"><span class="label"><?php _e( 'People: ' , 'fumseck' ); ?></span><?php echo $batbelt_people_list ; ?></li>
 			<?php }; ?>
 		
 			<?php if ( $batbelt_project = get_field( '_project', get_the_ID(), true ) ) {?>
-			<li><span class="label project"><?php _e( 'Project: ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_project ); ?></li>
+			<li class="project"><span class="label"><?php _e( 'Project: ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_project ); ?></li>
 			<?php } ?>
 		</ul>
 	</aside>
--- content.php
+++ content.php
@@ -3,8 +3,8 @@
 	<header>
 		<h1><?php the_title(); ?></h1>
 		<ul class="byline">
-			<li><span class="label author"><?php _e( 'By ' , 'fumseck' ); ?></span><?php the_author(); ?></li>
-			<li><span class="label pub_date"><?php _e( 'Published on ' , 'fumseck' ); ?></span><time datetime="<?php echo esc_attr( get_the_date( 'c' ) ) ; ?>" pubdate><?php echo date_i18n( __('F j, Y', 'fumseck'), get_the_date('U') ) ; ?></time></li>
+			<li class="author"><span class="label"><?php _e( 'By ' , 'fumseck' ); ?></span><?php the_author(); ?></li>
+			<li class="pub_date"><span class="label"><?php _e( 'Published on ' , 'fumseck' ); ?></span><time datetime="<?php echo esc_attr( get_the_date( 'c' ) ) ; ?>" pubdate><?php echo date_i18n( __('F j, Y', 'fumseck'), get_the_date('U') ) ; ?></time></li>
 		</ul>
 	</header>
 	
@@ -25,22 +25,22 @@
 	
 	<div class="the-content"><?php the_content(); ?></div>
 	
-	<aside class="meta2">
-		<ul>
+	<aside>
+		<ul class="meta2">
 			<?php if ( $categories_list = get_the_category_list( __( ', ', 'fumseck' ) ) ) {?>
-			<li><span class="label categories"><?php _ex( 'In ' , 'in categories', 'fumseck' ); ?></span><?php echo $categories_list ; ?></li>
+			<li class="categories"><span class="label"><?php _ex( 'In ' , 'in categories', 'fumseck' ); ?></span><?php echo $categories_list ; ?></li>
 			<?php }; ?>
 		
 			<?php if ( $tags_list = get_the_tag_list( '', __( ', ', 'fumseck' ) ) ) { ?>
-			<li><span class="label topics"><?php _e( 'Topics: ' , 'fumseck' ); ?></span><?php echo $tags_list ?></li>
+			<li class="topics"><span class="label"><?php _e( 'Topics: ' , 'fumseck' ); ?></span><?php echo $tags_list ?></li>
 			<?php }; ?>
 		
 			<?php if ( $batbelt_project = get_field( '_project', get_the_ID(), true ) ) {?>
-			<li><span class="label project"><?php _e( 'Project: ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_project ); ?></li>
+			<li class="project"><span class="label"><?php _e( 'Project: ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_project ); ?></li>
 			<?php }; ?>
 		
 			<?php if ( $batbelt_event = get_field( '_event', get_the_ID(), true ) ) {?>
-			<li><span class="label event"><?php _e( 'Event: ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_event ); ?></li>
+			<li class="event"><span class="label"><?php _e( 'Event: ' , 'fumseck' ); ?></span><?php fumseck_linked_title( $batbelt_event ); ?></li>
 			<?php }; ?>
 		</ul>
 	</aside>
