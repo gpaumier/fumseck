af82f20dee0d5e2592825c619b332df2b1fe8cb4
Guillaume Paumier <gp@guillaumepaumier.com>
2013-11-11 15:57:22 +0100

    Tweak the page title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<title><?php wp_title(); ?></title>
+		<title><?php wp_title('—', true, 'right'); ?></title>
 		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="profile" href="http://gmpg.org/xfn/11" />
