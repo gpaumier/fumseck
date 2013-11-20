2bbf511f899f6a86643f52ca44d90fc5dff85966
Guillaume Paumier <gp@guillaumepaumier.com>
2013-11-20 20:06:17 +0100

    Simplify syndication

--- syndication.php
+++ syndication.php
@@ -1,9 +1,10 @@
 			<li class="syndication"><i class="fa-li fa fa-share"></i> 
-				<?php printf( __('You can share this post on %s, %s or %s.', 'fumseck'),
-					'<a class="twitter" href="https://twitter.com/intent/tweet?url=' . get_permalink() . '&text=' . get_the_title() . '&via=gpaumier">twitter</a>',
-					'<a class="facebook" href="https://facebook.com/sharer.php?u=' . get_permalink() . '">facebook</a>',
-					'<a class="googleplus" href="https://plus.google.com/share?url=' . get_permalink() . '">Google+</a>' );
+				<?php 
+					$permalink = get_permalink();
+					printf( __('You can share this post on %s, %s, %s or elsewhere with the <a href="%s">permalink</a>.', 'fumseck'),
+					'<a class="twitter" href="https://twitter.com/intent/tweet?url=' . $permalink . '&text=' . get_the_title() . '&via=gpaumier">twitter</a>',
+					'<a class="facebook" href="https://facebook.com/sharer.php?u=' . $permalink . '">facebook</a>',
+					'<a class="googleplus" href="https://plus.google.com/share?url=' . $permalink . '">Google+</a>',
+					$permalink );
 				?>
 			</li>
-
-	
