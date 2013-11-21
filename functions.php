27f9eb30d18832560bf3a90c83eab4ebf46173e1
Guillaume Paumier <gp@guillaumepaumier.com>
2013-11-21 08:34:14 +0100

    Import function to work around wp_title issue

--- functions.php
+++ functions.php
@@ -114,4 +114,29 @@ function fumseck_list_languages() {
 	echo $output;
 }
 
+// Temporary workaround so the site name displays correctly ////////////////////
+
+function twentythirteen_wp_title( $title, $sep ) {
+	global $paged, $page;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'twentythirteen' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'twentythirteen_wp_title', 10, 2 );
+
+
 ?>
