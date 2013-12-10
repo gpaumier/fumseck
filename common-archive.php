3bf8316e10ae5416f295045ef7d0ea8c43c9db20
Guillaume Paumier <gp@guillaumepaumier.com>
2013-12-10 21:07:43 +0100

    Swap next/previous links to preserve sanity

--- common-archive.php
+++ common-archive.php
@@ -10,7 +10,7 @@
 
 	<div class="container">
 		<nav class="nav-links nav-full-width">
-			<div class="nav-link previous"><?php previous_posts_link('<i class="fa fa-caret-left"></i> ' . __( ' Previous entries' , 'fumseck' )) ?></div>
-			<div class="nav-link next"><?php next_posts_link(__( 'Next entries ' , 'fumseck' ) . '<i class="fa fa-caret-right"></i>') ?></div>
+			<div class="nav-link previous"><?php next_posts_link('<i class="fa fa-caret-left"></i> ' . __( ' Older entries' , 'fumseck' )) ?></div>
+			<div class="nav-link next"><?php previous_posts_link(__( 'Newer entries ' , 'fumseck' ) . '<i class="fa fa-caret-right"></i>') ?></div>
 		</nav>
 	</div>
--- single-batbelt_project.php
+++ single-batbelt_project.php
@@ -62,8 +62,8 @@
 
 	<div class="container">
 		<nav class="nav-links">
-			<div class="nav-link previous"><?php previous_post_link('%link', '<i class="fa fa-caret-left"></i> ' . __( 'Previous project ' , 'fumseck' )) ?></div>
-			<div class="nav-link next"><?php next_post_link('%link', __( 'Next project ' , 'fumseck' ) . '<i class="fa fa-caret-right"></i>') ?></div>
+			<div class="nav-link previous"><?php next_post_link('%link', '<i class="fa fa-caret-left"></i> ' . __( 'Previous project ' , 'fumseck' )) ?></div>
+			<div class="nav-link next"><?php previous_post_link('%link', __( 'Next project ' , 'fumseck' ) . '<i class="fa fa-caret-right"></i>') ?></div>
 		</nav>
 	</div>
 
