605bba15e5649e9b58316587918cdc2065934249
Guillaume Paumier <gp@guillaumepaumier.com>
2015-07-26 08:15:01 -0700

    add 'underway' status for page
    if the activity report is underway, don't link to it, and add a specific class. The implementation isn't pretty, but this is a stopgap measure until the static version is completed, so it just has to work

--- snippet-batbelt_project.php
+++ snippet-batbelt_project.php
@@ -1,21 +1,32 @@
-<?php	if ( $start_date = get_field( '_start_date', get_the_ID(), true ) ) { 
+<?php	if ( $start_date = get_field( '_start_date', get_the_ID(), true ) ) {
 			$start_date_fmtd = '<time datetime="' . esc_attr( $start_date ) . '">' . date_i18n( __('F j, Y', 'fumseck'), strtotime( $start_date ) ) . '</time>';
 			$meta_info_fmtd = sprintf( __( ' started on %s' , 'fumseck' ), $start_date_fmtd );
-			
-			if ( $end_date = get_field( '_end_date', get_the_ID(), true ) ) { 
+
+			if ( $end_date = get_field( '_end_date', get_the_ID(), true ) ) {
 				$end_date_fmtd = '<time datetime="' . esc_attr( $end_date ) . '">' . date_i18n( __('F j, Y', 'fumseck'), strtotime( $end_date ) ) . '</time>';
 				$meta_info_fmtd = $meta_info_fmtd . sprintf( __( ' and completed on %s' , 'fumseck' ), $end_date_fmtd );
 			} elseif ( $status = get_field_object('_status') ) {
 				$meta_info_fmtd = $meta_info_fmtd . sprintf( __( ' and %s' , 'fumseck' ), $status['choices'][ get_field('_status') ] );
 			}
-		}; ?>
+		};
+
+		$underway = get_field( '_underway', get_the_ID(), true );
+		?>
 
 <?php if ( $post_thumbnail = has_post_thumbnail() ) {  ?>
 
+	<?php if ( $underway ) {   ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('container snippet snippet-batbelt_project has-picture project-underway'); ?>>
+
+	<?php } else {   ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class('container snippet snippet-batbelt_project has-picture'); ?>>
-	
+
 	<a href="<?php echo esc_url( get_permalink()); ?>">
-	
+
+	<?php }; ?>
+
 	<header class="before">
 		<h1 class="snippet-title"><?php the_title(); ?></h1>
 		<p class="pub_date">
@@ -32,22 +43,30 @@
 			<i class="fa fa-tasks"></i> <span class="meta-label"><?php _e( 'Project' , 'fumseck' );  echo $meta_info_fmtd ?></span>
 		</p>
 	</header>
-		
+
 <?php } else { ?>
-	
+
+	<?php if ( $underway ) {   ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('container snippet snippet-batbelt_project no-picture project-underway'); ?>>
+
+	<?php } else {   ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class('container snippet snippet-batbelt_project no-picture'); ?>>
-	
+
 	<a class="snippet" href="<?php echo esc_url( get_permalink()); ?>">
-	
+
+	<?php }; ?>
+
 	<header>
 		<h1 class="snippet-title"><?php the_title(); ?></h1>
 		<p class="pub_date">
 			<i class="fa fa-tasks"></i> <span class="meta-label"><?php _e( 'Project ' , 'fumseck' );  echo $meta_info_fmtd ?></span>
 		</p>
 	</header>
-		
+
 <?php }; ?>
-		
+
 	<div class="summary">
 		<?php // Display the excerpt unless there's a summary
 			if ( $summary = get_field( '_summary', get_the_ID(), true ) ) {
@@ -56,9 +75,8 @@
 				the_excerpt();
 			}; ?>
 	</div>
-	
+
+	<?php if ( ! $underway ) {   ?>
 	</a>
+	<?php }; ?>
 </article><!-- #post -->
-
-
-
